e3b2754367625e8985fe3b44e0b037a63de1259b
yurayarosh <yura12323@gmail.com>
2024-05-11 09:46:05 +0300

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { getList } = require('./getList');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
