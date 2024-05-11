be19486863d51fe47170fb52bebc650c26ec774b
yurayarosh <yura12323@gmail.com>
2024-05-11 10:06:20 +0300

    change getting list logic

--- getList.js
+++ getList.js
@@ -1,7 +1,7 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
+const puppeteer = require('puppeteer');
+require('dotenv').config();
 
-const getList = async res => {
+const getList = async () => {
   const browser = await puppeteer.launch({
     headless: 'new',
     args: ['--disable-setuid-sandbox', '--no-sandbox', '--no-zygote'],
@@ -10,6 +10,7 @@ const getList = async res => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+  let list = [];
   try {
     const page = await browser.newPage();
 
@@ -20,15 +21,15 @@ const getList = async res => {
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    const list = await page.$$eval('.appendix-item__link-group > a', links =>
+    list = await page.$$eval('.appendix-item__link-group > a', links =>
       links.map(link => link.innerText)
     );
-    res.status(200).json(list);
   } catch (e) {
     console.log(e);
     res.status(500).json(`Something went wrong while running Puppeteer: ${e}`);
   } finally {
     await browser.close();
+    return list;
   }
 };
 
--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+let list = [];
+const millisecondsInDay = 1000 * 60 * 60 * 24;
+
+(async () => {
+  list = await getList();
+})();
+
+setInterval(async () => {
+  list = await getList();
+}, millisecondsInDay);
 
 app.get('/list', (req, res) => {
-  getList(res);
+  list.length > 0 ? res.status(200).json(list) : res.status(500).json("Couldn't get the list");
 });
 
 app.get('/', (req, res) => {
